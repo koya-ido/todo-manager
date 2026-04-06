c5ab8e9d64b1b273a105d7e18314372cbc4834a0
koya-ido-adglobe-co-jp <koya.ido@adglobe.co.jp>
2026-04-06 14:42:25 +0900

    セッション切れダイアログを追加

--- frontend/features/login/components/Content.tsx
+++ frontend/features/login/components/Content.tsx
@@ -5,11 +5,17 @@ import { LoginForm } from "@/features/login/components/LoginForm";
 import { ContentProps } from "@/types/contentTypes";
 import Link from "next/link";
 import { FC } from "react";
+import Image from "next/image";
 
 export const Content: FC<ContentProps> = ({ messages }) => {
   return (
     <div className="flex flex-col w-full items-center justify-center gap-6">
-      <img src="/assets/icons/ApplicationLogo.svg" alt="アプリケーションロゴ" />
+      <Image
+        src="/assets/icons/ApplicationLogo.svg"
+        alt="アプリケーションロゴ"
+        width={40}
+        height={40}
+      />
       <section className="flex flex-col items-center gap-2">
         <Heading level={1} className="text-2xl font-bold">
           TODO Manager
